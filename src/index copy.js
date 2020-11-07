a7f9349fb61d631991d58c396a3389617e0cac56
zhouxiaofei <zhouxiaofei@duliday.com>
2020-11-07 15:49:49 +0800

    #webpack autoprefixer 版本兼容

--- src/index copy.js
+++
@@ -1,8 +0,0 @@
-import './css/index.scss';
-import './css/index.css';
-import './image/media/iconfont.css';
-
-const arr = [new Promise(() => {}), new Promise(() => {})];
-arr.map((item) => {
-  console.log(item);
-});
---
+++ src/indexCopy.js
@@ -0,0 +1,10 @@
+import './css/index.scss';
+import './css/index.css';
+import './image/media/iconfont.css';
+
+/*
+ * const arr = [new Promise(() => {}), new Promise(() => {})];
+ * arr.map((item) => {
+ *   console.log(item);
+ * });
+ */
